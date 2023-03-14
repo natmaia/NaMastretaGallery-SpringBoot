37e152f8b6f2592ed78ce317351eee90cc24d24b
natmaia <devmaia@outlook.com>
2023-03-13 21:13:22 -0300

    Iniciando Sprint

--- src/main/java/br/com/fiap/NaMastreta/controller/HelloController.java
+++ src/main/java/br/com/fiap/NaMastreta/controller/HelloController.java
@@ -1,14 +1,16 @@
 package br.com.fiap.NaMastreta.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/app")
 public class HelloController {
 
-    @GetMapping(value="/")
-    public String getnome(){
-        return "Olá Mundo";
+    @GetMapping
+    public String hello(){
+        return "Olá Mundinho";
     }
     
 }
