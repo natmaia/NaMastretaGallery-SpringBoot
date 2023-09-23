44cad82eef59355965a6805a2c10aac78723ecd0
JaelsonJonas <livedojonas@hotmail.com>
2023-09-22 21:17:58 -0300

    API de imagem

---
+++ src/main/java/br/com/fiap/NaMastreta/controller/ImageController.java
@@ -0,0 +1,84 @@
+package br.com.fiap.namastreta.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import br.com.fiap.namastreta.DTO.Image;
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/api/image")
+public class ImageController {
+
+    @Value("${image.upload.directory}")
+    private String imageUploadDirectory;
+
+    @Autowired
+    private Environment environment;
+
+    @PostMapping("/upload")
+    public ResponseEntity<Image> uploadImage(@RequestParam("file") MultipartFile file, HttpServletRequest request)
+            throws IOException {
+        // Verifique se o diretório de upload de imagens existe, caso contrário, crie-o.
+        File uploadDir = new File(imageUploadDirectory);
+        if (!uploadDir.exists()) {
+            uploadDir.mkdirs();
+        }
+
+        // Gere um nome único para a imagem.
+        String imageName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+
+        // Salve a imagem no diretório de upload.
+        Path imagePath = Path.of(imageUploadDirectory, imageName);
+        Files.copy(file.getInputStream(), imagePath, StandardCopyOption.REPLACE_EXISTING);
+
+        String host = ServletUriComponentsBuilder.fromRequestUri(request)
+                .replacePath(null)
+                .build()
+                .toUriString();
+
+        // Construa a URL completa para a imagem.
+        String imageUrl = host + "/api/image/" + imageName;
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(new Image(imageUrl));
+    }
+
+    @GetMapping("/{imageName:.+}")
+    public ResponseEntity<byte[]> getImage(@PathVariable String imageName) throws IOException {
+        // Carregue a imagem a partir do diretório de upload.
+        String imageUploadDirectory = environment.getProperty("image.upload.directory");
+        Path imagePath = Path.of(imageUploadDirectory, imageName);
+
+        if (Files.exists(imagePath)) {
+            byte[] imageBytes = Files.readAllBytes(imagePath);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.IMAGE_JPEG);
+
+            return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+}
